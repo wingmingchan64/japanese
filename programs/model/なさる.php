26e5cca2050d5f6a0c67cdb0651afe3a2f93195d
Wing Ming Chan <wingmingchan64@hotmail.com>
2025-01-02 17:56:24 -0600

    Updated

---
+++ programs/model/なさる.php
@@ -0,0 +1,14 @@
+<?php
+$なさる=array(
+'Present'=>"${stem}る,${stem}います,${stem}らない,${stem}いません",
+'Past'=>"${stem}った,${stem}いました,${stem}らなかった,${stem}いませんでした",
+'-te Form'=>"${stem}って,${stem}らなくて",
+'Volitional'=>"${stem}ろう,${stem}いましょう",
+'Potential'=>"${stem}れる,${stem}れます,${stem}れない,${stem}れません",
+'Passive'=>"${stem}られる,${stem}られます,${stem}られない,${stem}られません",
+'Causative'=>"${stem}らせる,${stem}らせます,${stem}らせない,${stem}らせません",
+'Imperative'=>"${stem}い,${stem}ってください,${stem}るな,${stem}らないでください",
+'Condition'=>"${stem}れば,${stem}なければ",
+'Condition (-tara)'=>"${stem}ったら,${stem}らなかったら",
+);
+?>
---
+++ programs/model/呼ぶ.php
@@ -0,0 +1,14 @@
+<?php
+$呼ぶ=array(
+'Present'=>"${stem}ぶ,${stem}びます,${stem}ばない,${stem}びません",
+'Past'=>"${stem}んだ,${stem}びました,${stem}ばなかった,${stem}びませんでした",
+'-te Form'=>"${stem}んで,${stem}ばなくて",
+'Volitional'=>"${stem}ぼう,${stem}びましょう",
+'Potential'=>"${stem}べる,${stem}べます,${stem}べない,${stem}べません",
+'Passive'=>"${stem}ばれる,${stem}ばれます,${stem}ばれない,${stem}ばれません",
+'Causative'=>"${stem}ばせる,${stem}ばせます,${stem}ばせない,${stem}ばせません",
+'Imperative'=>"${stem}べ,${stem}んでください,${stem}ぶな,${stem}ばないでください",
+'Condition'=>"${stem}べば,${stem}べなければ",
+'Condition (-tara)'=>"${stem}んだら,${stem}ばなかったら",
+);
+?>
