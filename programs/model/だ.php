b3102b4074b95765f8b91b06331180264b90b845
Wing Ming Chan <wingmingchan64@hotmail.com>
2025-01-18 10:08:26 -0600

    Updated

--- programs/model/だ.php
+++ programs/model/だ.php
@@ -6,5 +6,6 @@ $だ=array(
 'Volitional'=>"だろう,でしょう",
 'Condition'=>"であれば,でなければ",
 'Condition (-tara)'=>"だったら,ではなかったら/じゃなかったら",
+'Notes'=>'だ is not the plain form of です See https://japanese.stackexchange.com/questions/22939/is-%e3%81%a0-plain-form-of-%e3%81%a7%e3%81%99-omittable#22943',
 );
 ?>
